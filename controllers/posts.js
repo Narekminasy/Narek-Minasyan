21c692d956853977ee17d549384969464e9ffc1e
Narek Minasyan <narekminasyan52@gmail.com>
2026-05-08 15:35:42 +0400

    i add functions CRUD

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,45 @@
+let posts = [];
+
 export default {
-    async create(req,res,next) {},
-    async getAll(req,res,next) {},
-    async update(req,res,next) {},
-    async delete(req,res,next) {}
+    async create(req, res, next) {
+        try {
+            const post = {
+                id: Date.now(),
+                title: req.body.title,
+            };
+
+            posts.push(post);
+
+            res.json(post);
+        } catch (e) {
+            next(e);
+        }
+    },
+    async getAll(req, res, next) {
+        try {
+            res.json(posts);
+        } catch (e) {
+            next(e);
+        }
+    },
+    async update(req,res,next) {
+        try {
+            const {id}= req.body;
+            const post = posts.find(p => p.id === id);
+            if (!post) {
+                return res.status(404).json({error:"not found"});
+            }
+        }catch (e){
+         next(e);
+        }
+    },
+    async delete(req, res, next) {
+        try {
+            const { id } = req.body;
+            posts = posts.filter(p => p.id != id);
+            res.json({ message: "Deleted" });
+        } catch (e) {
+            next(e);
+        }
+    }
 }
